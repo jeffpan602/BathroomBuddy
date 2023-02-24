9f8d7dd1fd609c8af924201567de6894a78729d7
Jeffrey Pan <107944513+jeffpan602@users.noreply.github.com>
2023-02-23 21:34:51 -0500

    setup bathroom db

--- backend/bathroom/admin.py
+++ backend/bathroom/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import bathroom
+
+
+class bathroomAdmin(admin.ModelAdmin):
+    list_display = ('id', 'building_id', 'longitude', 'latitude', 'floor',
+                    'room_number', 'num_toilets', 'num_urinals', 'num_sinks', 'gender')
+
+# Register your models here.
+
+
+admin.site.register(bathroom, bathroomAdmin)
